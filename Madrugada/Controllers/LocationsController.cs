12ef8d0baa04c47b4eba5a466a781a6221859727
mjyn <mjyn@live.cn>
2019-01-02 00:42:47 +0800

    basic scafs

---
+++ Madrugada/Controllers/LocationsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Madrugada.Data;
+using Madrugada.Models;
+
+namespace Madrugada.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Locations
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Locations.Include(l => l.Work);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Locations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .Include(l => l.Work)
+                .FirstOrDefaultAsync(m => m.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        public IActionResult Create()
+        {
+            ViewData["WorkId"] = new SelectList(_context.Works, "WorkId", "Name");
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LocationId,Name,WorkId,Text,Address,Latitude,Longitude,HasStreetView,StreetViewLatitude,StreetViewLongitude,StreetViewHeading,StreetViewPitch,IsPublished")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["WorkId"] = new SelectList(_context.Works, "WorkId", "Name", location.WorkId);
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            ViewData["WorkId"] = new SelectList(_context.Works, "WorkId", "Name", location.WorkId);
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LocationId,Name,WorkId,Text,Address,Latitude,Longitude,HasStreetView,StreetViewLatitude,StreetViewLongitude,StreetViewHeading,StreetViewPitch,IsPublished")] Location location)
+        {
+            if (id != location.LocationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(location.LocationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["WorkId"] = new SelectList(_context.Works, "WorkId", "Name", location.WorkId);
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .Include(l => l.Work)
+                .FirstOrDefaultAsync(m => m.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.LocationId == id);
+        }
+    }
+}
---
+++ Madrugada/Controllers/WorksController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Madrugada.Data;
+using Madrugada.Models;
+
+namespace Madrugada.Controllers
+{
+    public class WorksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WorksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Works
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Works.Include(w => w.Company);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Works/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works
+                .Include(w => w.Company)
+                .FirstOrDefaultAsync(m => m.WorkId == id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+
+            return View(work);
+        }
+
+        // GET: Works/Create
+        public IActionResult Create()
+        {
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "CompanyId", "Name");
+            return View();
+        }
+
+        // POST: Works/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WorkId,Name,AlternativeName,ReleaseDate,Cover,OfficialHomepage,CompanyId,TagsString")] Work work)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(work);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "CompanyId", "Name", work.CompanyId);
+            return View(work);
+        }
+
+        // GET: Works/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works.FindAsync(id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "CompanyId", "Name", work.CompanyId);
+            return View(work);
+        }
+
+        // POST: Works/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WorkId,Name,AlternativeName,ReleaseDate,Cover,OfficialHomepage,CompanyId,TagsString")] Work work)
+        {
+            if (id != work.WorkId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(work);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkExists(work.WorkId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "CompanyId", "Name", work.CompanyId);
+            return View(work);
+        }
+
+        // GET: Works/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works
+                .Include(w => w.Company)
+                .FirstOrDefaultAsync(m => m.WorkId == id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+
+            return View(work);
+        }
+
+        // POST: Works/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var work = await _context.Works.FindAsync(id);
+            _context.Works.Remove(work);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkExists(int id)
+        {
+            return _context.Works.Any(e => e.WorkId == id);
+        }
+    }
+}
